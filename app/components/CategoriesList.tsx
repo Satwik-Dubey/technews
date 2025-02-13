4c93e7e15196f433fbf6d740692498e9ed36868f
SatwikDubey <dubeysatwik0@gmail.com>
2025-02-13 09:57:42 +0530

    added categories

---
+++ app/components/CategoriesList.tsx
@@ -0,0 +1,22 @@
+import { CategoriesData } from "@/data";
+import Link from "next/link";
+
+
+export default function CategoriesList() {
+
+
+    return (
+        <div className="flex gap-2 text-sm flex-wrap">
+        {CategoriesData &&
+          CategoriesData.map((category) => (
+            <Link
+              key={category.id}
+              className="px-4 py-1 rounded-md bg-slate-800 text-white cursor-pointer"
+              href={`/categories/${category.name}`}
+            >
+              {category.name}
+            </Link>
+            ))}
+        </div>
+    );
+}
